dc799c798ef5f145a7780ab9522864714964b51b
iamfareedshaik <iamfareedshaik@gmail.com>
2024-07-16 22:19:22 -0500

    initial commit

--- src/api.js
+++ src/api.js
@@ -24,6 +24,16 @@ export const addTodo = async (todo) => {
   }
 };
 
+export const updateTodo = async (id, updatedTodo) => {
+  try {
+    const response = await axios.put(`${API_URL}${id}/`, updatedTodo);
+    return response.data;
+  } catch (error) {
+    console.error(`Error updating todo with id ${id}:`, error);
+    throw error;
+  }
+};
+
 export const deleteTodo = async (id) => {
   try {
     const response = await axios.delete(`${API_URL}${id}/`);
